7881228c099453008e107feac8bcf99fb54cd5a7
ilynne <ilynne@gmail.com>
2015-05-29 12:02:08 -0700

    adds ip-based before filter

--- app/controllers/postmark_controller.rb
+++ app/controllers/postmark_controller.rb
@@ -1,2 +1,14 @@
 class PostmarkController < ApplicationController
+  before_filter :require_postmark_ip
+
+  def bounce_handler
+    render :nothing => true, :status => 200
+  end
+
+  def require_postmark_ip
+    unless request.env["REMOTE_ADDR"] == '1.2.3.4'
+      render :nothing => true, :status => 401
+    end
+  end
+
 end
--- spec/controllers/postmark_controller_spec.rb
+++ spec/controllers/postmark_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PostmarkController, type: :controller do
-  let(:api_token) { '12345678abcdefgh'}
+  # let(:json) {d => '{ "ID": 42, "Type": "HardBounce", "TypeCode": 1, "Name": "Hard bounce", "Tag": "Test", "MessageID": "883953f4-6105-42a2-a16a-77a8eac79483", "Description": "The server was unable to deliver your message (ex: unknown user, mailbox not found).", "Details": "Test bounce details", "Email": "john@example.com", "BouncedAt": "2014-08-01T13:28:10.2735393-04:00", "DumpAvailable": true, "Inactive": true, "CanActivate": true, "Subject": "Test subject" }'}
+
   describe 'a request from an invalid server' do
-    it 'should not be processed' do
-      post 'bounce_handler', d => { "ID": 42, "Type": "HardBounce", "TypeCode": 1, "Name": "Hard bounce", "Tag": "Test", "MessageID": "883953f4-6105-42a2-a16a-77a8eac79483", "Description": "The server was unable to deliver your message (ex: unknown user, mailbox not found).", "Details": "Test bounce details", "Email": "john@example.com", "BouncedAt": "2014-08-01T13:28:10.2735393-04:00", "DumpAvailable": true, "Inactive": true, "CanActivate": true, "Subject": "Test subject" }
-      puts response.status
+    it 'should return an unauthorized response' do
+      post 'bounce_handler', format: :json, :d => { "ID" => 42 }
+      expect(response.status).to eq(401)
+    end
+  end
+
+  describe 'a request from an authorized server' do
+    it 'should accept the request' do
+      request.env['REMOTE_ADDR'] = '1.2.3.4'
+      post 'bounce_handler', format: :json, :d => { "ID" => 42 }
+      expect(response.status).to eq(200)
     end
   end
 end
